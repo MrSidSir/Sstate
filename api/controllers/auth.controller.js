ab396962a4b6c11e8ba1480cfc34165a02031cc7
MrSidSir <irshad1554@gmail.com>
2025-06-04 00:30:32 +0530

    added profile page

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -25,7 +25,7 @@ export const signin = async (req, res, next) => {
     if (!isPasswordCorrect)
       return next(errorHandler(401, 'Invalid email or password'));
 
-    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    const token = jwt.sign({ id: user._id }, 'irshad_secret_key_123');
 
     const { password: pass, ...rest } = user._doc;
     res
